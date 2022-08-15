c721e1c72f5e09ddca88a554d5c5b026f943ebe3
sangji <parksangji1109@gmail.com>
2022-08-15 20:04:48 +0900

    [Create] Custom Login Success Handler

--- src/main/java/com/comdolidoli/security/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/comdolidoli/security/handler/CustomLoginSuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,HttpServletResponse response, Authentication authentication) throws IOException{
         SecurityContextHolder
